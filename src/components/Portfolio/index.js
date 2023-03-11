ff5c773acc1b9086be2db975150ffaa0690490f4
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-11 13:59:18 -0500

    Added style.css for resume

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,7 @@ import forecast from "../../assets/small/5-day-forcast.png";
 import albania from "../../assets/small/albanian quiz.png";
 import estate from "../../assets/small/real estate.png";
 
+
 function Portfolio() {
   return (
     <div>
@@ -13,7 +14,7 @@ function Portfolio() {
 
           <div className="col-md-6">
             <div>
-              <a href="https://github.com/alkidavaci/sports-app">
+              <a href="https://github.com/alkidavaci/sports-app" >
                 <img
                   src={sport}
                   className="my-2 img-fluid rounded"
@@ -23,14 +24,7 @@ function Portfolio() {
               <div>
                 <h4>MARV'S SPORTS</h4>
                 <p>
-                  MARV'S SPORTS is a live streaming website which is used to
-                  retrieve sports data. The user has to select a soccer league
-                  from the leagues dropdown, then the user has to choose a team
-                  from the teams dropdown. Then the user has to select a date
-                  from the calendar to retrieve chosen day's data. The news
-                  headlines, scores & upcoming matches are also displayed side
-                  by side. Used Moment.js library to work with date. Used Rapid
-                  API to retrieve match data for teams.
+                  MARV'S SPORTS is a live streaming website which is used to retrieve sports data. The user has to select a soccer league from the leagues dropdown, then the user has to choose a team from the teams dropdown. Then the user has to select a date from the calendar to retrieve chosen day's data. The news headlines, scores & upcoming matches are also displayed side by side. Used Moment.js library to work with date. Used Rapid API to retrieve match data for teams.
                 </p>
               </div>
             </div>
@@ -48,12 +42,7 @@ function Portfolio() {
               <div>
                 <h4>5 Day Forecast</h4>
                 <p>
-                  Day Forecast website is application that allows a user to see
-                  the weather outlook for multiple cities. User search for a
-                  city name and will view future weather conditions for that
-                  city for current day and 5 day forecast. Displays the date, an
-                  icon representation of weather conditions, the temperature,
-                  the wind speed, and the humidity.{" "}
+                  Day Forecast website is application that allows a user to see the weather outlook for multiple cities. User search for a city name and will view future weather conditions for that city for current day and 5 day forecast. Displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity. {" "}
                 </p>
               </div>
             </div>
@@ -71,14 +60,7 @@ function Portfolio() {
               <div>
                 <h4>Albanian Quiz</h4>
                 <p>
-                  Albania, located in southern Europe is a country that not
-                  everyone know. This website is a quiz how much you know
-                  Albania. Has 5 questions total and they are all with 4 choice.
-                  The time limit for question is 60 seconds and for each wrong
-                  answer will penalize with 10 seconds less. After you finish
-                  the quiz will show your scores. You can put your initial and
-                  save the scores if they are on the top 5. Also you can access
-                  your high scores and clear them. Enjoy the quiz!{" "}
+                  Albania, located in southern Europe is a country that not everyone know. This website is a quiz how much you know Albania. Has 5 questions total and they are all with 4 choice. The time limit for question is 60 seconds and for each wrong answer will penalize with 10 seconds less. After you finish the quiz will show your scores. You can put your initial and save the scores if they are on the top 5. Also you can access your high scores and clear them. Enjoy the quiz!{" "}
                 </p>
               </div>
             </div>
@@ -95,19 +77,14 @@ function Portfolio() {
               <div>
                 <h4>4 Real Estate</h4>
                 <p>
-                  This is a real-estate listing website with seeded listings
-                  from our SQL database. Users can login or create an account to
-                  create new listings, and once submitted, their listing appears
-                  on the Home Page. Conditional logic shows login or logout/add
-                  listing buttons, and Cloudinary is used to store house
-                  pictures.{" "}
+                  This is a real-estate listing website with seeded listings from our SQL database. Users can login or create an account to create new listings, and once submitted, their listing appears on the Home Page. Conditional logic shows login or logout/add listing buttons, and Cloudinary is used to store house pictures.{" "}
                 </p>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
-  );
+      </ section>
+    </div >)
 }
 export default Portfolio;
+
