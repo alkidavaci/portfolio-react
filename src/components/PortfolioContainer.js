d1ddcdbdf9d606f9357c0fdf4a4f4dd8070b92ca
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-10 20:29:34 -0500

    Return the corresponding component About, Portfolio, Contact and Resume

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // Check the value of `currentPage` and return the corresponding component to render.
+  const renderPage = () => {
+    
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* Passing currentPage from state and the function handlePageChange to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Method to return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
