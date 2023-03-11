a387d3fee968c2967d6d848dc8f1c52230bdf3b9
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-11 14:57:10 -0500

    Added style.css for Portofolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,35 +3,44 @@ import sport from "../../assets/small/MARV'S SPORTS.png";
 import forecast from "../../assets/small/5-day-forcast.png";
 import albania from "../../assets/small/albanian quiz.png";
 import estate from "../../assets/small/real estate.png";
+import './style.css'
 
 
 function Portfolio() {
   return (
     <div>
       <section>
-        <div className="row">
-          <h2 className="section-title secondary-border col-12 mb-4">Work</h2>
+        <div className="row p-3">
+          <h1 className="section-title text-center p-3">Work</h1>
 
-          <div className="col-md-6">
-            <div>
-              <a href="https://github.com/alkidavaci/sports-app" >
-                <img
-                  src={sport}
-                  className="my-2 img-fluid rounded"
-                  alt="MARV'S SPORTS"
-                />
-              </a>
-              <div>
-                <h4>MARV'S SPORTS</h4>
-                <p>
-                  MARV'S SPORTS is a live streaming website which is used to retrieve sports data. The user has to select a soccer league from the leagues dropdown, then the user has to choose a team from the teams dropdown. Then the user has to select a date from the calendar to retrieve chosen day's data. The news headlines, scores & upcoming matches are also displayed side by side. Used Moment.js library to work with date. Used Rapid API to retrieve match data for teams.
-                </p>
+          <div className="col-md-12">
+            <div className="project">
+              <h4>MARV'S SPORTS</h4>
+              <div className="row">
+                <div className="col-md-4">
+                  <a href="https://github.com/alkidavaci/sports-app" >
+                    <img
+                      src={sport}
+                      className="my-2 img-fluid rounded"
+                      alt="MARV'S SPORTS"
+                    />
+                  </a>
+                </div>
+                <div className="project col-md-8">
+                  <p>
+                    MARV'S SPORTS is a live streaming website which is used to retrieve sports data. The user has to select a soccer league from the leagues dropdown, then the user has to choose a team from the teams dropdown. Then the user has to select a date from the calendar to retrieve chosen day's data. The news headlines, scores & upcoming matches are also displayed side by side. Used Moment.js library to work with date. Used Rapid API to retrieve match data for teams.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="col-md-6">
-            <div className="job">
+
+          <div className="col-md-12">
+            <div className="project">
+              <h4>5 Day Forecast</h4>
+              <div className="row">
+                <div className="col-md-4">
               <a href="https://github.com/alkidavaci/5-day-forecast">
                 <img
                   src={forecast}
@@ -39,17 +48,22 @@ function Portfolio() {
                   alt="5 Day Forecast"
                 />
               </a>
-              <div>
-                <h4>5 Day Forecast</h4>
+              </div>
+                <div className="project col-md-8">
                 <p>
                   Day Forecast website is application that allows a user to see the weather outlook for multiple cities. User search for a city name and will view future weather conditions for that city for current day and 5 day forecast. Displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity. {" "}
-                </p>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="col-md-6">
-            <div>
+
+          <div className="col-md-12">
+            <div className="project">
+            <h4>Albania Quiz</h4>
+            <div className="row">
+                <div className="col-md-4">
               <a href="https://github.com/alkidavaci/Albania-Quiz">
                 <img
                   src={albania}
@@ -57,16 +71,21 @@ function Portfolio() {
                   alt="Albanian Quiz"
                 />
               </a>
-              <div>
-                <h4>Albanian Quiz</h4>
+              </div>
+                <div className="project col-md-8">
                 <p>
                   Albania, located in southern Europe is a country that not everyone know. This website is a quiz how much you know Albania. Has 5 questions total and they are all with 4 choice. The time limit for question is 60 seconds and for each wrong answer will penalize with 10 seconds less. After you finish the quiz will show your scores. You can put your initial and save the scores if they are on the top 5. Also you can access your high scores and clear them. Enjoy the quiz!{" "}
-                </p>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-md-6">
-            <div>
+          
+          <div className="col-md-12">
+            <div className="project">
+              <h4>4 Real Estate</h4>
+              <div className="row">
+                <div className="col-md-4">
               <a href="https://github.com/alkidavaci/4-Real-Estate">
                 <img
                   src={estate}
@@ -74,11 +93,12 @@ function Portfolio() {
                   alt="4 Real Estate"
                 />
               </a>
-              <div>
-                <h4>4 Real Estate</h4>
+              </div>
+                <div className="project col-md-8">
                 <p>
                   This is a real-estate listing website with seeded listings from our SQL database. Users can login or create an account to create new listings, and once submitted, their listing appears on the Home Page. Conditional logic shows login or logout/add listing buttons, and Cloudinary is used to store house pictures.{" "}
-                </p>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
